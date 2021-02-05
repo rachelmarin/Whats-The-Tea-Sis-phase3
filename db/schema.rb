c4a0a436f73de4deb9cce3059bc706d3d1668e60
rachelmarin <Rachelmarinpal@gmail.com>
2021-02-05 16:15:09 -0500

    created category controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_204758) do
+ActiveRecord::Schema.define(version: 2021_02_05_210649) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
