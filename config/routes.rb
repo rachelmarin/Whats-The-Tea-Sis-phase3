fcc32cc2678d4f8d51da7b1681c1b329de2d734a
rachelmarin <Rachelmarinpal@gmail.com>
2021-02-05 14:54:57 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  root(to: "static#home")
+    
+   
+    get "/signup", to: "users#new", as: "signup"
+    post "/signup", to: "users#create"
+    get "/login", to: "sessions#new", as: "login"
+    post "/login", to: "sessions#create"
+  
+    delete "/logout", to: "sessions#destroy", as: "destroy_user_session"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
