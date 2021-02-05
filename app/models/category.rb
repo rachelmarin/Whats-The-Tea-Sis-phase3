7001cc91afb32e340abd5acb878af4357d139751
rachelmarin <Rachelmarinpal@gmail.com>
2021-02-05 16:12:59 -0500

    category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,12 @@
 class Category < ApplicationRecord
+has_many :user
+has_many :recipes
+
+validates :name, presence: true
+
+accepts_nested_attributes_for :recipes, reject_if: :all_blank  
+
+def self.alphabetize
+    order(name: :asc)
+  end
 end
